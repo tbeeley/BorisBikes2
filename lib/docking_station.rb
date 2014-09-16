4ef5b3fb6dd8aa6a4f7039b7af7e22a46a4e9508
Thomas Beeley <tbeeley@gmail.com>
2014-09-16 14:34:59 +0100

    capacity added

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,10 +1,11 @@
 class DockingStation
 
-	def initialize
+	def initialize(capacity = 20)
 		@bikes = []
+		@capacity = capacity
 	end
 
-	attr_accessor :bikes
+	attr_accessor :bikes, :capacity
 
 	def bike_count
 		@bikes.count
@@ -12,6 +13,14 @@ class DockingStation
 
 	def accept(bike)
 		@bikes << bike
+	end
+
+	def release(bike)
+		@bikes.delete(bike)
+	end
+
+	def full?
+		bike_count == capacity
 	end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,13 +2,24 @@ require 'docking_station'
 
 describe 'DockingStation' do
 
-	let(:docking_station) { DockingStation.new }
+	let(:station) { DockingStation.new }
 	let(:bike) 			  { Bike.new		   }
 
 	context 'when created' do
 
+
+
 		it 'should not have any bikes' do
-			expect(docking_station.bike_count).to eq 0
+			expect(station.bike_count).to eq 0
+		end
+
+		it 'should have a capacity' do
+			expect(station.capacity).to eq 20
+		end
+
+		it 'should know when it is full' do
+			20.times { station.accept(bike) }
+			expect(station.full?).to be true
 		end
 
 	end
@@ -16,8 +27,16 @@ describe 'DockingStation' do
 	context 'docking and releasing' do
 
 		it 'should be able to dock a bike' do
-			docking_station.accept(bike)
-			expect(docking_station.bike_count).to eq 1
+			station.accept(bike)
+			expect(station.bike_count).to eq 1
 		end
+
+		it 'should be able to release a bike' do
+			station.accept(bike)
+			station.release(bike)
+			expect(station.bike_count).to eq 0
+		end
+
 	end
+
 end
