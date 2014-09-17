9a35b8eeed8f846c15e47037abadf5aee60c0b02
Thomas Beeley <tbeeley@gmail.com>
2014-09-17 15:50:05 +0100

    Docking Station refactored

--- spec/bike_container_spec.rb
+++ spec/bike_container_spec.rb
@@ -1,82 +1,84 @@
 require 'bike_container'
-require 'bike'
-
-class ContainerHolder; include BikeContainer
-
-	describe BikeContainer do
-
-		let(:container) 	{ ContainerHolder.new }
-		let(:working_bike) { double :bike, broken?: false }
-		let(:broken_bike) { double :bike, broken?: true }
 
 
-		context 'when created' do
+# class ContainerHolder; include BikeContainer
 
-			it 'should not have any bikes' do
-				expect(container.bike_count).to eq 0
-			end
+# 	describe BikeContainer do
 
-			it 'should have a capacity' do
-				expect(container.capacity).to eq 20
-			end
+shared_examples 'a bike container' do
 
+	let(:container) 	{ described_class.new }
+	let(:working_bike) { double :bike, broken?: false }
+	let(:broken_bike) { double :bike, broken?: true }
+
+
+	context 'when created' do
+
+		it 'should not have any bikes' do
+			expect(container.bike_count).to eq 0
 		end
 
-		context 'bike container' do
-
-			it 'should know when it is full' do
-				20.times { container.accept(working_bike) }
-				expect(container.full?).to be true
-			end
-
-			it 'should know when it is empty' do
-				expect(container.bike_count).to eq 0
-				expect(container.empty?).to eq true
-			end
-
-			it 'should know how many bikes are available' do
-				container.accept(working_bike)
-				container.accept(broken_bike)
-				expect(container.available_bikes).to eq 1
-			end
-
-			it 'should have a list of working bikes' do
-				container.accept(working_bike)
-				container.accept(broken_bike)
-				expect(container.working_bikes).to eq [working_bike]
-			end
-
-			it 'should have a list of broken bikes' do
-				container.accept(working_bike)
-				container.accept(broken_bike)
-				expect(container.broken_bikes).to eq [broken_bike]
-			end
-
+		it 'should have a capacity' do
+			expect(container.capacity).to eq 20
 		end
 
-		context 'docking and releasing' do
+	end
 
-			it 'should be able to dock a bike' do
-				container.accept(working_bike)
-				expect(container.bike_count).to eq 1
-			end
+	context 'bike container' do
 
-			it 'should be able to release a bike' do
-				container.accept(working_bike)
-				container.release(working_bike)
-				expect(container.bike_count).to eq 0
-			end
+		it 'should know when it is full' do
+			20.times { container.accept(working_bike) }
+			expect(container.full?).to be true
+		end
 
-			it 'should not accept a bike if full' do
-				20.times { container.accept(working_bike) }
-				expect(lambda { container.accept(working_bike) }).to raise_error('This container is full')
-			end
+		it 'should know when it is empty' do
+			expect(container.bike_count).to eq 0
+			expect(container.empty?).to eq true
+		end
 
-			it 'should not release a bike if empty' do
-				expect(lambda { container.release(working_bike) }).to raise_error('There are no available bikes')
-			end
+		it 'should know how many bikes are available' do
+			container.accept(working_bike)
+			container.accept(broken_bike)
+			expect(container.available_bikes).to eq 1
+		end
 
+		it 'should have a list of working bikes' do
+			container.accept(working_bike)
+			container.accept(broken_bike)
+			expect(container.working_bikes).to eq [working_bike]
+		end
+
+		it 'should have a list of broken bikes' do
+			container.accept(working_bike)
+			container.accept(broken_bike)
+			expect(container.broken_bikes).to eq [broken_bike]
+		end
+
+	end
+
+	context 'docking and releasing' do
+
+		it 'should be able to dock a bike' do
+			container.accept(working_bike)
+			expect(container.bike_count).to eq 1
+		end
+
+		it 'should be able to release a bike' do
+			container.accept(working_bike)
+			container.release(working_bike)
+			expect(container.bike_count).to eq 0
+		end
+
+		it 'should not accept a bike if full' do
+			20.times { container.accept(working_bike) }
+			expect(lambda { container.accept(working_bike) }).to raise_error('This container is full')
+		end
+
+		it 'should not release a bike if empty' do
+			expect(lambda { container.release(working_bike) }).to raise_error('There are no available bikes')
 		end
 
 	end
 end
+# 	end
+# end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,10 +3,11 @@ require 'docking_station'
 describe DockingStation do
 
 	let(:station) { DockingStation.new  }
-	let(:bike) 			  { Bike.new	}
-	let(:broken_bike) { Bike.new.break! }
+
+	it_behaves_like 'a bike container'
 
 	it 'should not release a bike if broken' do
+		broken_bike = double :bike, broken?: true
 		station.accept(broken_bike)
 		expect(lambda { station.release(broken_bike) }).to raise_error('This bike is broken')
 	end
